33770ea00a6d4d6e4bcf344ce8b79449b47c6296
Pramod <Pramod.Voola@gmail.com>
2020-06-17 17:35:59 -0400

    First Commit for Kotlin Language Learning

---
+++ src/com/rsk/Person.kt
@@ -0,0 +1,11 @@
+package com.rsk
+
+class Person (var Name: String){
+
+    fun display(){
+
+        println("Display: $Name")
+
+    }
+
+}
---
+++ src/main.kt
@@ -0,0 +1,14 @@
+
+import com.rsk.Person
+
+fun main(args: Array<String>){
+    println("Hello World")
+
+    val kevin = Person("Kevin")
+
+
+    println("Name is ${kevin.Name}")
+
+    kevin.display()
+
+}
